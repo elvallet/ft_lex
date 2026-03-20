77f2a333f2c7de88e36eef7e82daf9cdfddde9dc
Elsa Vallet <elsa.vallethilarion@live.com>
2026-03-20 09:44:08 +0100

    Thompson

--- src/automata/Thompson.cpp
+++ src/automata/Thompson.cpp
@@ -1,4 +1,172 @@
 #include "Thompson.hpp"
+#include <stack>
+#include <stdexcept>
 
-using namespace automata;
+using namespace automata; using namespace std;
 
+NFA Thompson::compile(const vector<Token>& postfixe) {
+	stack<Fragment>	worklist;
+
+	for (const Token& t : postfixe) {
+		switch (t.type_)
+		{
+		case CHAR:
+		{
+			worklist.push(make_literal(t.value_));
+			break;
+		}
+		case CONCAT:
+		{
+			if (worklist.size() < 2) throw runtime_error("Invalid regex");
+
+			Fragment b	= worklist.top();
+			worklist.pop();
+
+			Fragment a	= worklist.top();
+			worklist.pop();
+
+			worklist.push(make_concat(a, b));
+			break;
+		}
+		case UNION:
+		{
+			if (worklist.size() < 2) throw runtime_error("Invalid regex");
+
+			Fragment a	= worklist.top();
+			worklist.pop();
+
+			Fragment b	= worklist.top();
+			worklist.pop();
+
+			worklist.push(make_union(a, b));
+			break;
+		}
+		case STAR:
+		{
+			if (worklist.empty()) throw runtime_error("Invalid regex");
+
+			Fragment a = worklist.top();
+			worklist.pop();
+
+			worklist.push(make_star(a));
+			break;
+		}
+		case PLUS:
+		{
+			if (worklist.empty()) throw runtime_error("Invalid regex");
+
+			Fragment a = worklist.top();
+			worklist.pop();
+
+			worklist.push(make_plus(a));
+			break;
+		}
+		case QUESTION:
+		{	
+			if (worklist.empty()) throw runtime_error("Invalid regex");
+
+			Fragment a = worklist.top();
+			worklist.pop();
+
+			worklist.push(make_question(a));
+			break;
+		}
+		default:
+			throw runtime_error("Invalid token");
+		}
+	}
+
+	if (worklist.empty()) throw runtime_error("Invalid regex");
+	Fragment	frag	= worklist.top();
+	int 		final	= add_state();
+
+	patch(frag.out_, final);
+	nfa_.initial_state_	= frag.start_;
+	nfa_.final_states_	= {final};
+
+	return nfa_;
+}
+
+int Thompson::add_state() {
+	int	id = nfa_.transitions_.size();
+
+	nfa_.transitions_.push_back({});
+	nfa_.epsilon_transitions_.push_back({});
+
+	return id;
+}
+
+void Thompson::patch(vector<DanglingOut>&out, int target) {
+	for (const auto &o : out) {
+		if (o.is_epsilon_) {
+			nfa_.epsilon_transitions_[o.state_].push_back(target);
+		} else {
+			nfa_.transitions_[o.state_][o.c_].push_back(target);
+		}
+	}
+}
+
+Fragment Thompson::make_literal(char c) {
+	int first_state		= add_state();
+	int second_state	= add_state();
+	vector<int>	v;
+
+	v.push_back(second_state);
+	nfa_.transitions_[first_state][c]	= v;
+	nfa_.alphabet_.insert(c);
+
+	Fragment frag;
+	frag.start_	= first_state;
+	frag.out_.push_back({second_state, true, 0});
+
+	return frag;
+}
+
+Fragment Thompson::make_concat(Fragment a, Fragment b) {
+	patch(a.out_, b.start_);
+
+	return Fragment{a.start_, b.out_};
+}
+
+Fragment Thompson::make_union(Fragment a, Fragment b) {
+	int s	= add_state();
+
+	nfa_.epsilon_transitions_[s].push_back(a.start_);
+	nfa_.epsilon_transitions_[s].push_back(b.start_);
+
+	a.out_.insert(a.out_.end(), b.out_.begin(), b.out_.end());
+	return Fragment{s, std::move(a.out_)};
+}
+
+Fragment Thompson::make_star(Fragment a) {
+	int s	= add_state();
+
+	nfa_.epsilon_transitions_[s].push_back(a.start_);
+	patch(a.out_, s);
+
+	vector<DanglingOut>	v;
+	v.push_back({s, true, 0});
+
+	return Fragment{s, v};
+}
+
+Fragment Thompson::make_plus(Fragment a) {
+	int s	= add_state();
+
+	nfa_.epsilon_transitions_[s].push_back(a.start_);
+	patch(a.out_, s);
+
+	vector<DanglingOut>	v;
+	v.push_back({s, true, 0});
+
+	return Fragment{a.start_, v};
+}
+
+Fragment Thompson::make_question(Fragment a) {
+	int s	= add_state();
+
+	nfa_.epsilon_transitions_[s].push_back(a.start_);
+
+	a.out_.push_back({s, true, 0});
+	return Fragment{s, std::move(a.out_)};
+}
--- src/automata/Thompson.hpp
+++ src/automata/Thompson.hpp
@@ -1,9 +1,40 @@
 #pragma once
 
+#include <vector>
+
 #include "NFA.hpp"
+#include "Token.hpp"
+
 
 namespace automata {
 
-// Thompson -> regex to NFA
+struct DanglingOut {
+	int		state_;
+	bool	is_epsilon_;
+	char	c_;
+};
+
+struct Fragment {
+	int							start_;
+	std::vector<DanglingOut>	out_;
+};
+
+class Thompson {
+public:
+	NFA compile(const std::vector<Token>& postfixe);
+
+private:
+	NFA	nfa_;
+
+	int		add_state();
+	void	patch(std::vector<DanglingOut>& out, int target);
+
+	Fragment	make_literal(char c);
+	Fragment	make_concat(Fragment a, Fragment b);
+	Fragment	make_union(Fragment a, Fragment b);
+	Fragment	make_star(Fragment a);
+	Fragment	make_plus(Fragment a);
+	Fragment	make_question(Fragment a);
+};
 
 } // namespace automata
